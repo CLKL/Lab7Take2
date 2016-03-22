0aa735051236e0a28c302c8ea6e2824b235763bc
kelly70545 <kelly70545@hotmail.com>
2016-03-22 12:12:27 -0700

    KL: Modified timetable.php model Signed-off-by:kelly70545 <kelly70545@hotmail.com>

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -9,69 +9,54 @@
 class TimeTable extends CI_Model {
 
     protected $xml = null;
-    protected $course = array();
-    protected $dayOfWeek = array();
-    protected $period = array();
+    protected $course = array(); // courses facet
+    protected $dayOfWeek = array(); // days facet
+    protected $period = array(); // period facet
 
     function __construct() {
         parent::_construct();
-        $this->xml = simplexml_load_file(DATAPATH . 'timetable.xml');
+        // Load XML File
+        $this->xml = simplexml_load_file(DATAPATH . 'master.xml', "SimpleXMLElement", LIBXML_NOENT);
 
-        // build a full list of courses
+
+        // build a full list of bookings by courses
         foreach ($this->xml->courses->course as $course) {
-            $record = new stdClass();
-            $record->code = (string) $course['code'];
-            $record->name = (string) $course;
-            $this->couse[$record->code] = $record;
-            //$this->course_name[(string) $course[course_num]] = (string) $course;
+            foreach ($course->booking as $booking) {
+                $this->courses[] = new Booking($booking, $course);
+            }
         }
 
-        // build a full list of days
+        // build a full list of bookings by days
         foreach ($this->xml->days->day as $day) {
-            $record = new stdClass();
-            $record->code = (string) $day['code'];
-            $record->name = (string) $day;
-            $this->days[$record->code] = $record;
-        }
-
-        // build a full list of period
-        foreach ($this->xml->periods->preiod as $period) {
-            $record = new stdClass();
-            $record->code = (string) $period['code'];
-            $record->time = (string) $period;
-            $this->days[$record->code] = $record;
-        }
-
-        // retrieve a list of courses, to populate a dropdown, for instance
-        function course() {
-            return $this->course;
-        }
-
-        // retrieve a course record
-        function getCourse($code) {
-            if (isset($this->course[$code])) {
-                return $this->course[$code];
-            } else {
-                return null;
+            foreach ($day->booking as $booking) {
+                $this->days[] = new Booking($booking, $day);
             }
         }
 
-        // retrieve a day record
-        function getDay($code) {
-            if (isset($this->day[$code])) {
-                return $this->day[$code];
-            } else {
-                return null;
+        // build a full list of bookings by periods
+        foreach ($this->xml->periods->period as $period) {
+            foreach ($period->booking as $booking) {
+                $this->periods[] = new Booking($booking, $period);
             }
         }
 
-        // retrieve a period record
-        function getPeriod($code) {
-            if (isset($this->period[$code])) {
-                return $this->period[$code];
-            } else {
-                return null;
-            }
+        //----------------------------------------------------------------------
+        // Public Accessors
+        //----------------------------------------------------------------------
+        
+        // retrieves php array for bookings from course.xml
+        function getCourses() {
+            return $this->courses;
+        }
+
+        // retrieves php array for bookings from dayOfWeek.xml
+        function getDays() {
+            return $this->days;
+        }
+
+       // retrieves php array for bookings from period.xml
+        function getPeriods($code) {
+            return $this->periods;
         }
 
     }
